eb2f58719765b3a5263117e1bfc713b1d9abd7b9
imanishbarnwal <manishbarnwal725@gmail.com>
2021-03-30 23:44:38 +0530

    Tictactoe Updated

--- tictactoe.py
+++ tictactoe.py
@@ -57,7 +57,7 @@ def CheckWin():
     else:            
         Game=Running    
     
-print("Tic-Tac-Toe Game Designed By Sourabh Somani")    
+print("Tic-Tac-Toe Game Designed By Manish")    
 print("Player 1 [X] --- Player 2 [O]\n")    
 print()    
 print()    
